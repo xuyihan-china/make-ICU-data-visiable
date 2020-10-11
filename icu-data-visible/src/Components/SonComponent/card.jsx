920beecc1df46f583976f4adf7915bca6520bbc5
xuyihan-china <848247256@qq.com>
2020-10-11 08:56:55 +0800

    00

--- icu-data-visible/src/Components/SonComponent/card.jsx
+++ icu-data-visible/src/Components/SonComponent/card.jsx
@@ -18,24 +18,21 @@ class card extends Component {
         let num = Math.ceil(Math.random() * 15 + 1)
         setInterval(() => {
             num = Math.ceil(Math.random() * 15 + 1)
-        }, 1);
+        }, 3);
         let heartArray = [70, 71, 92, 94, 95, 96, 97, 98,100,111,100,66,65,44,144,170,70, 71, 92, 94,]
         let breathArray = [88,89,67,68,69,78,99,100,44,78,91,92,93,94,95,77,75,111,100,66,65,44]
         let bloodArray = [130,128,88,160,90,100,120,111,113,111,100,66,65,44,144,170,111,113,114,115]
-        setInterval(() => {
-            let heartRate = randomNum(90, 100)
-            heartArray.push(heartRate)
-            let breath = randomNum(60, 100)
-            breathArray.push(breath)
-            let blood = randomNum(130, 140)
-            bloodArray.push(blood)
-        }, 200);
-        setTimeout(()=>{
-            window.location.reload();
-         },3000); 
-            
-         
-         
+        // setInterval(() => {
+        //     let heartRate = randomNum(90, 100)
+        //     heartArray.push(heartRate)
+        //     let breath = randomNum(60, 100)
+        //     breathArray.push(breath)
+        //     let blood = randomNum(130, 140)
+        //     bloodArray.push(blood)
+        // }, 2000);
+        // setTimeout(()=>{
+        //     window.location.reload();
+        //  },3000); 
         return (
             <div>
                 
@@ -46,7 +43,6 @@ class card extends Component {
                         "ICU patient"
                     }
                 >
-                   
                     <p style={{ fontWeight: 'bold',color:'red' }}>心跳:{heartArray[num]}次/min</p>
                     <p style={{ fontWeight: 'bold',color:'skyblue' }}>血压:{bloodArray[num]}mmHg</p>
                     <p style={{ fontWeight: 'bold',color:'black'}}>呼吸:{breathArray[num]}次/min</p>
